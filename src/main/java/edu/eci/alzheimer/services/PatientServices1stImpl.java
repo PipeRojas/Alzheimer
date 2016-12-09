edbf0d280b11c7778e2cb345aefd3a1e3dffcda3
AlejandraZamora <alejandra26.az@gmail.com>
2016-12-09 15:23:28 -0500

    Datos quemados

--- src/main/java/edu/eci/alzheimer/services/PatientServices1stImpl.java
+++ src/main/java/edu/eci/alzheimer/services/PatientServices1stImpl.java
@@ -22,8 +22,260 @@ public class PatientServices1stImpl implements PatientServices{
         resumeId.setGameName("Sopa Letras");
         resumeId.setLevelReached(7);
         resumeId.setTimeSpendMilisenconds(new Long(30000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Maria Camila");
+        resumeId.setPatientLastName("Rodriguez Velez");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(8);
+        resumeId.setTimeSpendMilisenconds(new Long(50000));
         resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Maria Camila");
+        resumeId.setPatientLastName("Rodriguez Velez");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(4);
+        resumeId.setTimeSpendMilisenconds(new Long(60000));
+        resumesId.add(resumeId);
+        //
         resumes.put(123456789, resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Oscar Andres");
+        resumeId.setPatientLastName("Castro Luna");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(15);
+        resumeId.setTimeSpendMilisenconds(new Long(80000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Oscar Andres");
+        resumeId.setPatientLastName("Castro Luna");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(9);
+        resumeId.setTimeSpendMilisenconds(new Long(60000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Oscar Andres");
+        resumeId.setPatientLastName("Castro Luna");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(3);
+        resumeId.setTimeSpendMilisenconds(new Long(30000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(987654321,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Fabian David");
+        resumeId.setPatientLastName("Gomez Lopez");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(6);
+        resumeId.setTimeSpendMilisenconds(new Long(20000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Fabian David");
+        resumeId.setPatientLastName("Gomez Lopez");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(13);
+        resumeId.setTimeSpendMilisenconds(new Long(100000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Fabian David");
+        resumeId.setPatientLastName("Gomez Lopez");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(8);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(125698745,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Andrea Carolina");
+        resumeId.setPatientLastName("Ruiz Vargas");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(1);
+        resumeId.setTimeSpendMilisenconds(new Long(8000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Andrea Carolina");
+        resumeId.setPatientLastName("Ruiz Vargas");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(8);
+        resumeId.setTimeSpendMilisenconds(new Long(70000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Andrea Carolina");
+        resumeId.setPatientLastName("Ruiz Vargas");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(9);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(36598445,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Juan David");
+        resumeId.setPatientLastName("Suarez Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(23);
+        resumeId.setTimeSpendMilisenconds(new Long(500000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Juan David");
+        resumeId.setPatientLastName("Suarez Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(9);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Juan David");
+        resumeId.setPatientLastName("Suarez Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(3);
+        resumeId.setTimeSpendMilisenconds(new Long(70000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(548965412,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Camila Andrea");
+        resumeId.setPatientLastName("Aguirre Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(8);
+        resumeId.setTimeSpendMilisenconds(new Long(80000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Camila Andrea");
+        resumeId.setPatientLastName("Aguirre Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(16);
+        resumeId.setTimeSpendMilisenconds(new Long(200000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Camila Andrea");
+        resumeId.setPatientLastName("Aguirre Reyes");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(7);
+        resumeId.setTimeSpendMilisenconds(new Long(70000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(311254595,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Julian Andres");
+        resumeId.setPatientLastName("Avila Fonseca");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(9);
+        resumeId.setTimeSpendMilisenconds(new Long(60000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Julian Andres");
+        resumeId.setPatientLastName("Avila Fonseca");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(12);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Julian Andres");
+        resumeId.setPatientLastName("Avila Fonseca");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(9);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(455878951,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Camilo Andres");
+        resumeId.setPatientLastName("Hernandez Vargas");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Concentrate");
+        resumeId.setLevelReached(5);
+        resumeId.setTimeSpendMilisenconds(new Long(70000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Camilo Andres");
+        resumeId.setPatientLastName("Hernandez Vargas");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(8);
+        resumeId.setTimeSpendMilisenconds(new Long(90000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(95455423,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Angie Valeria");
+        resumeId.setPatientLastName("Blanco Cardona");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(6);
+        resumeId.setTimeSpendMilisenconds(new Long(60000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Angie Valeria");
+        resumeId.setPatientLastName("Blanco Cardona");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(17);
+        resumeId.setTimeSpendMilisenconds(new Long(100000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(75548426,resumesId);
+        //
+        resumesId=new ArrayList<>();
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Felipe Andres");
+        resumeId.setPatientLastName("Rojas Oritz");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Diferencias");
+        resumeId.setLevelReached(1);
+        resumeId.setTimeSpendMilisenconds(new Long(9000));
+        //
+        resumeId=new DataPOJO();
+        resumeId.setPatientName("Felipe Andres");
+        resumeId.setPatientLastName("Rojas Oritz");
+        resumeId.setRegDate(new Date());
+        resumeId.setGameName("Sopa Letras");
+        resumeId.setLevelReached(2);
+        resumeId.setTimeSpendMilisenconds(new Long(20000));
+        resumesId.add(resumeId);
+        //
+        resumes.put(210102258,resumesId);
     }
 
     @Override
